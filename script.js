3ef302d8767a6f55a74fe8ec5d764c76a9eb7706
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-28 23:51:52 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+documnet =addEventListener("DOMContentLoaded",function(){
+	const targetElement = document.getElementById("level");
+	if(!targetElement){
+		alert("The element with id 'level' does not exist.");
+		return;
+	}
+
+	let domLevel =0;
+	let currentElement = targetElement;
+
+	while (currentElement) {
+		domLevel++;
+		currentElement=currentElement.parentElement;
+	}
+	alert(`The level of element is : ${'domLevel'}`);
+}); 
+	
+
